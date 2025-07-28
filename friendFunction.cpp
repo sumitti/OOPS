5bd3840a82e22d19d27e32ee5e36004a078c9a00
sumitti <st023429@gmail.com>
2025-07-28 22:38:52 +0530

    Overloading of Operator as a friend function

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -13,10 +13,18 @@ class Integer{
     cout<<"A:: "<<a<<" B:: "<<b<<endl;
   }
   friend void fun(Integer);
+  friend Integer operator + (Integer,Integer); // Overloading of operator as Friend Function
 };
 
 void fun(Integer c){
     cout<<"SUM:: "<<c.a+c.b<<endl;
+}
+
+Integer operator + (Integer X, Integer Y){
+    Integer temp;
+    temp.a = X.a + Y.a;
+    temp.b = X.b + Y.b;
+    return temp;
 }
 
 int main(){
@@ -24,5 +32,10 @@ int main(){
     i1.setData(4,5);
     i1.showData();
     fun(i1);
+    Integer i2,i3,i4;
+    i2.setData(4,5);
+    i3.setData(1,1);
+    i4 = i2 + i3;
+    i4.showData();
     return 0;
 }
