8caf7ec916cfa5fdb1f037abfb1c45f6316f3ff7
sumitti <st023429@gmail.com>
2025-07-29 12:50:14 +0530

    Insertion & Extraction operator Overloading using friend function

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -13,8 +13,29 @@ class Integer{
     cout<<"A:: "<<a<<" B:: "<<b<<endl;
   }
   friend void fun(Integer);
-  friend Integer operator + (Integer,Integer); // Overloading of operator as Friend Function
+  friend Integer operator + (Integer,Integer); // Overloading of Binary operator as Friend Function
 };
+
+class Complex{
+  private:
+    int a, b;
+  public:
+      
+    friend ostream& operator <<(ostream& , Complex);
+    friend istream& operator >>(istream& , Complex&);
+};
+
+ostream& operator << (ostream& output, Complex C){
+  output<<C.a<<" "<<C.b<<endl;
+  return(output); // return isiliye kara rahay hai taki cascading hosakay;
+}
+
+istream& operator >> (istream& input, Complex &C){
+  input>>C.a>>C.b;  
+  return(input); // return isiliye kara rahay hai taki cascading hosakay;
+}
+
+
 
 void fun(Integer c){
     cout<<"SUM:: "<<c.a+c.b<<endl;
@@ -28,14 +49,21 @@ Integer operator + (Integer X, Integer Y){
 }
 
 int main(){
-    Integer i1;
-    i1.setData(4,5);
-    i1.showData();
-    fun(i1);
-    Integer i2,i3,i4;
-    i2.setData(4,5);
-    i3.setData(1,1);
-    i4 = i2 + i3;
-    i4.showData();
+    // Integer i1;
+    // i1.setData(4,5);
+    // i1.showData();
+    // fun(i1);
+    // Integer i2,i3,i4;
+    // i2.setData(4,5);
+    // i3.setData(1,1);
+    // i4 = i2 + i3;
+    // i4.showData();
+
+    Complex c1,c2;
+    cout<<"Enter the Complex Number: ";
+    cin>>c1;
+    cin>>c2;
+    cout<<c1<<endl;
+    cout<<c2<<endl;
     return 0;
 }
